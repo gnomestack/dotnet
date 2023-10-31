d043dcf489bf14ddb9523d3d4761b81f1ed05aff
bowtiedgnome <119234633+bowtiedgnome@users.noreply.github.com>
2023-10-31 04:12:29 -0500

    update ci workflow for testing 6

--- .eng/tasks/deps.ts
+++ .eng/tasks/deps.ts
@@ -1,7 +1,7 @@
 export {task, parseAndRun} from "https://deno.land/x/qtr@0.0.10/mod.ts";
-export * from "https://deno.land/x/quasar@0.0.7/os/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.7/process/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.7/fs/scripting.ts";
-export * from "https://deno.land/x/quasar@0.0.7/path/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.6/os/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.6/process/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.6/fs/scripting.ts";
+export * from "https://deno.land/x/quasar@0.0.6/path/scripting.ts";
 export * as dotenv from "https://deno.land/std@0.200.0/dotenv/mod.ts";
 
